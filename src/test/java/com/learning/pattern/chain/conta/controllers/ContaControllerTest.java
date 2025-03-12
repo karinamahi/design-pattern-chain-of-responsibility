6386ea1981e1c29458a8791bb05e928f7a630bf2
karinamahi <karinahiratabueno@gmail.com>
2025-03-12 18:38:04 +0000

    🎨  formated tests

--- src/test/java/com/learning/pattern/chain/conta/controllers/ContaControllerTest.java
+++ src/test/java/com/learning/pattern/chain/conta/controllers/ContaControllerTest.java
@@ -6,25 +6,25 @@ import com.learning.pattern.chain.conta.models.Conta;
 import com.learning.pattern.chain.conta.models.Format;
 
 public class ContaControllerTest {
-    
+
     @Test
-    public void deveImprimirXml(){
-	ContaController controller = new ContaController();
-	Conta conta = new Conta("Karina", 1.0);
-	controller.imprimeConta(Format.XML, conta);
+    public void deveImprimirXml() {
+        ContaController controller = new ContaController();
+        Conta conta = new Conta("Karina", 1.0);
+        controller.imprimeConta(Format.XML, conta);
     }
-    
+
     @Test
-    public void deveImprimirCsv(){
-	ContaController controller = new ContaController();
-	Conta conta = new Conta("Karina", 1.0);
-	controller.imprimeConta(Format.CSV, conta);
+    public void deveImprimirCsv() {
+        ContaController controller = new ContaController();
+        Conta conta = new Conta("Karina", 1.0);
+        controller.imprimeConta(Format.CSV, conta);
     }
-    
+
     @Test
-    public void deveImprimirPorcento(){
-	ContaController controller = new ContaController();
-	Conta conta = new Conta("Karina", 1.0);
-	controller.imprimeConta(Format.PERCENT, conta);
+    public void deveImprimirPorcento() {
+        ContaController controller = new ContaController();
+        Conta conta = new Conta("Karina", 1.0);
+        controller.imprimeConta(Format.PERCENT, conta);
     }
 }
--- src/test/java/com/learning/pattern/chain/desconto/controllers/CalculadorDeDescontosTest.java
+++ src/test/java/com/learning/pattern/chain/desconto/controllers/CalculadorDeDescontosTest.java
@@ -10,54 +10,53 @@ import com.learning.pattern.chain.desconto.models.Orcamento;
 public class CalculadorDeDescontosTest {
 
     private CalculadorDeDescontos calculador;
-    
+
     @BeforeClass
-    public void init(){
-	this.calculador = new CalculadorDeDescontos();
-    }
-    
-    @Test
-    public void deveCalcularDesconto(){
-	Orcamento orcamento = new Orcamento(500.0);
-	orcamento.adicionaItem(new Item("caneta", 250.0));
-	orcamento.adicionaItem(new Item("livro", 250.0));
-	double desconto = this.calculador.calcula(orcamento);
-	Assert.assertEquals(desconto, 0.0);
-	System.out.println("Desconto: " + desconto);
-    }
-    
-    @Test
-    public void deveCalcularDescontoPorMaisDeCincoItens(){
-	Orcamento orcamento = new Orcamento(500.0);
-	orcamento.adicionaItem(new Item("caneta", 50.0));
-	orcamento.adicionaItem(new Item("livro", 50.0));
-	orcamento.adicionaItem(new Item("caderno", 50.0));
-	orcamento.adicionaItem(new Item("apontador", 50.0));
-	orcamento.adicionaItem(new Item("tesoura", 50.0));
-	orcamento.adicionaItem(new Item("régua", 50.0));
-	double desconto = this.calculador.calcula(orcamento);
-	System.out.println("Desconto: " + desconto);
-	Assert.assertTrue(desconto > 0);
-    }
-    
-    @Test
-    public void deveCalcularDescontoPorMaisDeQuinhentosReais(){
-	Orcamento orcamento = new Orcamento(600.0);
-	orcamento.adicionaItem(new Item("caneta", 50.0));
-	orcamento.adicionaItem(new Item("caderno", 50.0));
-	double desconto = this.calculador.calcula(orcamento);
-	System.out.println("Desconto: " + desconto);
-	Assert.assertTrue(desconto > 0);
-    }
-    
-    @Test
-    public void deveCalcularDescontoPorVendaCasada(){
-	Orcamento orcamento = new Orcamento(500.0);
-	orcamento.adicionaItem(new Item("caneta", 10.0));
-	orcamento.adicionaItem(new Item("lapis", 10.0));
-	double desconto = this.calculador.calcula(orcamento);
-	System.out.println("Desconto: " + desconto);
-	Assert.assertTrue(desconto > 0);
+    public void init() {
+        this.calculador = new CalculadorDeDescontos();
     }
 
+    @Test
+    public void deveCalcularDesconto() {
+        Orcamento orcamento = new Orcamento(500.0);
+        orcamento.adicionaItem(new Item("caneta", 250.0));
+        orcamento.adicionaItem(new Item("livro", 250.0));
+        double desconto = this.calculador.calcula(orcamento);
+        Assert.assertEquals(desconto, 0.0);
+        System.out.println("Desconto: " + desconto);
+    }
+
+    @Test
+    public void deveCalcularDescontoPorMaisDeCincoItens() {
+        Orcamento orcamento = new Orcamento(500.0);
+        orcamento.adicionaItem(new Item("caneta", 50.0));
+        orcamento.adicionaItem(new Item("livro", 50.0));
+        orcamento.adicionaItem(new Item("caderno", 50.0));
+        orcamento.adicionaItem(new Item("apontador", 50.0));
+        orcamento.adicionaItem(new Item("tesoura", 50.0));
+        orcamento.adicionaItem(new Item("régua", 50.0));
+        double desconto = this.calculador.calcula(orcamento);
+        System.out.println("Desconto: " + desconto);
+        Assert.assertTrue(desconto > 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoPorMaisDeQuinhentosReais() {
+        Orcamento orcamento = new Orcamento(600.0);
+        orcamento.adicionaItem(new Item("caneta", 50.0));
+        orcamento.adicionaItem(new Item("caderno", 50.0));
+        double desconto = this.calculador.calcula(orcamento);
+        System.out.println("Desconto: " + desconto);
+        Assert.assertTrue(desconto > 0);
+    }
+
+    @Test
+    public void deveCalcularDescontoPorVendaCasada() {
+        Orcamento orcamento = new Orcamento(500.0);
+        orcamento.adicionaItem(new Item("caneta", 10.0));
+        orcamento.adicionaItem(new Item("lapis", 10.0));
+        double desconto = this.calculador.calcula(orcamento);
+        System.out.println("Desconto: " + desconto);
+        Assert.assertTrue(desconto > 0);
+    }
 }
