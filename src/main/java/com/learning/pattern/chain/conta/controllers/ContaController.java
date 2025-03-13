b98e1d1c5168dc281f153944bbcfe4a733b82e77
karinamahi <karinahiratabueno@gmail.com>
2025-03-13 12:55:42 +0000

    🌍  translated XmlResponse

--- src/main/java/com/learning/pattern/chain/conta/controllers/ContaController.java
+++ src/main/java/com/learning/pattern/chain/conta/controllers/ContaController.java
@@ -7,7 +7,7 @@ import com.learning.pattern.chain.conta.resposta.Response;
 import com.learning.pattern.chain.conta.resposta.impl.ErrorResponse;
 import com.learning.pattern.chain.conta.resposta.impl.CsvResponse;
 import com.learning.pattern.chain.conta.resposta.impl.PercentResponse;
-import com.learning.pattern.chain.conta.resposta.impl.RespostaEmXml;
+import com.learning.pattern.chain.conta.resposta.impl.XmlResponse;
 
 public class ContaController {
     
@@ -16,7 +16,7 @@ public class ContaController {
 	Response erro = new ErrorResponse();
 	Response porcento = new PercentResponse(erro);
 	Response csv = new CsvResponse(porcento);
-	Response xml = new RespostaEmXml(csv);
+	Response xml = new XmlResponse(csv);
 	
 	Request request = new Request(format);
 	xml.reply(request, account);
