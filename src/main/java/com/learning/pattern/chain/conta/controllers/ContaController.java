036935a105ac98cea2c49997c1aba9e53d0c8a53
karinamahi <karinahiratabueno@gmail.com>
2025-03-13 11:34:24 +0000

    🌍  translated RespostaDeErro to ErrorResponse

--- src/main/java/com/learning/pattern/chain/conta/controllers/ContaController.java
+++ src/main/java/com/learning/pattern/chain/conta/controllers/ContaController.java
@@ -4,7 +4,7 @@ import com.learning.pattern.chain.conta.models.Account;
 import com.learning.pattern.chain.conta.models.Format;
 import com.learning.pattern.chain.conta.models.Request;
 import com.learning.pattern.chain.conta.resposta.Response;
-import com.learning.pattern.chain.conta.resposta.impl.RespostaDeErro;
+import com.learning.pattern.chain.conta.resposta.impl.ErrorResponse;
 import com.learning.pattern.chain.conta.resposta.impl.RespostaEmCsv;
 import com.learning.pattern.chain.conta.resposta.impl.RespostaEmPorcento;
 import com.learning.pattern.chain.conta.resposta.impl.RespostaEmXml;
@@ -13,7 +13,7 @@ public class ContaController {
     
     public void imprimeConta(Format format, Account account){
 	
-	Response erro = new RespostaDeErro();
+	Response erro = new ErrorResponse();
 	Response porcento = new RespostaEmPorcento(erro);
 	Response csv = new RespostaEmCsv(porcento);
 	Response xml = new RespostaEmXml(csv);
--- src/main/java/com/learning/pattern/chain/conta/resposta/impl/ErrorResponse.java
+++ src/main/java/com/learning/pattern/chain/conta/resposta/impl/ErrorResponse.java
@@ -4,11 +4,10 @@ import com.learning.pattern.chain.conta.models.Account;
 import com.learning.pattern.chain.conta.models.Request;
 import com.learning.pattern.chain.conta.resposta.Response;
 
-public class RespostaDeErro implements Response {
+public class ErrorResponse implements Response {
 
     @Override
     public void reply(Request request, Account account) {
-	System.out.println("Não foi possível imprimir a conta.");
+        System.out.println("The account could not be printed.");
     }
-
 }
